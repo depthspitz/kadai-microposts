0e3c990fa21004e22f338a83e728022f60c716fc
takumi.masajima <1996detective@gmail.com>
2021-06-21 03:37:49 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   include Pagy::Backend
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in? #unlessはifの反対。falseの時に処理を実行
+      redirect_to login_url
+    end
+  end
 end
